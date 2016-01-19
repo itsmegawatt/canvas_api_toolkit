4a4fc27a6b7f7560d910d6e90b24b0321a449329
Megg Gawat <megg@linux-aspp.site>
2016-01-19 17:24:54 -0500

    Added GUI to app with login menu

--- surveyorv3.py
+++ surveyorv3.py
@@ -26,6 +26,8 @@ import xml.etree.ElementTree as ET #for reading XML files
 import webbrowser #used for opening links in web browser
 import abc #abstract base class
 import collections #for ordered dict
+import tkinter #used for gui
+import tkinter.scrolledtext as tkst #used for gui textboxes with scrollbars
 
 class Login(object):
     def __init__(self, username=None, password=None):
@@ -66,7 +68,15 @@ class Forms_API(Canvas_API):
         total_forms = 0
         for form in root.iter('Form'):
             total_forms += 1
+        print(total_forms)
         return total_forms
+
+    def verify_login(self):
+        #Verifies if the login works by opening the XML file, and if the first child of the root is "Error", it is a failed login. If it works, it should be "Forms".
+        verifyroot = ET.fromstring(self.api_xml)
+        if verifyroot[0].tag == "Error":
+            return False
+        return True
 
 class Submissions_API(Canvas_API):
 
@@ -114,6 +124,163 @@ class Submissions_API(Canvas_API):
                     print((value + ': ' + str(dict_of_screens[screen][label][value]))) 
             print('')
 
-        print(dict_of_screens)
+        #print(dict_of_screens)
 
-        self.dict_of_screens = dict_of_screens
+        #self.dict_of_screens = dict_of_screens
+
+class Images_API(Canvas_API):
+    
+    def __init__(self, loginobj):
+        pass
+
+class Reference_Data_API(Canvas_API):
+    pass
+
+class CSV_Meta_Data_API(Canvas_API):
+    pass
+
+class CSV_API(Canvas_API):
+    pass
+
+class Dispatch_Items_API(Canvas_API):
+    pass
+
+class Submissions_Push_Notifications_API(Canvas_API):
+    pass
+
+class SurveyorInterface(object):
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Canvas Toolkit")
+        self.mainframe = tkinter.Frame(master)
+        self.mainframe.pack(padx=10, pady=10)
+
+        self.username = tkinter.StringVar()
+        self.password = tkinter.StringVar()
+        self.formid = tkinter.StringVar()
+        self.console_text = tkinter.StringVar()
+
+        self.build_login_gui()
+
+    def build_login_gui(self):
+        self.login_frame = tkinter.Frame(self.mainframe)
+        self.login_frame.grid(row = 0, column = 0)
+
+        canvas_toolkit_title = tkinter.Label(self.login_frame, text="Canvas Toolkit")
+        canvas_toolkit_title.grid(column = 0, row = 0, columnspan = 2)
+
+        username_text = tkinter.Label(self.login_frame, text = "Username")
+        username_text.grid(column = 0, row = 1)
+        username_box = tkinter.Entry(self.login_frame, textvariable = self.username)
+        username_box.grid(column = 0, row = 2)
+
+        password_text = tkinter.Label(self.login_frame, text = "Password")
+        password_text.grid(column = 1, row = 1)
+        password_box = tkinter.Entry(self.login_frame, textvariable = self.password, show="*")
+        password_box.grid(column = 1, row = 2)
+
+        login_button = tkinter.Button(self.login_frame, text="Login", command = self.attempt_login)
+        login_button.grid(column = 0, row = 4, columnspan = 2, pady=5)
+
+    def attempt_login(self):
+        self.account_info = Login(self.username.get(), self.password.get())
+        self.forms_api_login = Forms_API(self.account_info)
+
+        if self.forms_api_login.verify_login() == True:
+            self.login_frame.grid_forget()
+            self.build_forms_browser()
+            self.build_console()
+            self.build_buttons_kit()
+        else:
+            ##incorrect_login_message = tkinter.Label(self.login_frame, text = "Error: Incorrect Login")
+            ##incorrect_login_message.grid(column = 0, row = 3, columnspan = 2)
+            #Commented the above to make it easier to log in and test, otherwise remove in final versoin
+            self.login_frame.grid_forget()
+            self.build_forms_browser()
+            self.build_console()
+            self.build_buttons_kit()
+
+    def build_forms_browser(self):
+        forms_browser_frame = tkinter.Frame(self.mainframe)
+        forms_browser_frame.grid(column = 0, row = 1)        
+
+    def build_console(self):
+        console_frame = tkinter.Frame(self.mainframe)
+        console_frame.grid(column = 0, row = 2)
+
+    def build_buttons_kit(self):
+        buttons_kit_frame = tkinter.Frame(self.mainframe)
+        buttons_kit_frame.grid(column = 0, row = 3)
+
+        #-----Forms API Buttons-----#
+
+        forms_api_title = tkinter.Label(buttons_kit_frame, text = "Forms API")
+        forms_api_title.grid(column = 0, row = 0)
+
+        visit_forms_xml_button = tkinter.Button(buttons_kit_frame, text = "Visit XML", command = self.forms_api_login.visit_api)
+        visit_forms_xml_button.grid(column = 0, row = 1)
+
+        list_forms_button = tkinter.Button(buttons_kit_frame, text = "List Forms", command = self.forms_api_login.list_forms)
+        list_forms_button.grid(column = 0, row = 2)
+
+        count_forms_button = tkinter.Button(buttons_kit_frame, text = "Count Forms", command = self.forms_api_login.count_forms)
+        count_forms_button.grid(column = 0, row = 3)
+
+        #-----Submissions API Buttons-----#
+
+        submissions_api_title = tkinter.Label(buttons_kit_frame, text = "Submissions API")
+        submissions_api_title.grid(column = 1,row = 0)
+
+        tally_responses_entry = tkinter.Entry(buttons_kit_frame, textvariable = self.formid)
+        tally_responses_entry.grid(column = 1, row = 1)
+
+        visit_submissions_xml_button = tkinter.Button(buttons_kit_frame, text = "Visit XML", command = self.gui_visit_submissions_xml)
+        visit_submissions_xml_button.grid(column = 1, row = 2)
+
+        tally_responses_button = tkinter.Button(buttons_kit_frame, text = "Tally Responses", command = self.gui_tally_responses)
+        tally_responses_button.grid(column = 1, row = 3)
+
+        #-----Images API Buttons-----#
+        images_api_title = tkinter.Label(buttons_kit_frame, text = "Images API")
+        images_api_title.grid(column = 2, row = 0)
+
+
+        #-----Reference Data API Buttons-----#
+        reference_data_api_title = tkinter.Label(buttons_kit_frame, text = "Reference Data API")
+        reference_data_api_title.grid(column = 3, row = 0)
+
+
+        #-----CSV Meta Data API Buttons-----#
+        csv_meta_data_api_title = tkinter.Label(buttons_kit_frame, text = "CSV Meta Data API")
+        csv_meta_data_api_title.grid(column = 4, row = 0)
+
+
+        #-----CSV API Buttons-----#
+        csv_api_title = tkinter.Label(buttons_kit_frame, text = "CSV API")
+        csv_api_title.grid(column = 5, row = 0)
+
+
+        #-----Dispatch Items API Buttons-----#
+        dispatch_items_api_title = tkinter.Label(buttons_kit_frame, text = "Dispatch Items API")
+        dispatch_items_api_title.grid(column = 6, row = 0)
+
+
+        #-----Submissions Push Notifications API Buttons-----#
+        submissions_push_notifications_api_title = tkinter.Label(buttons_kit_frame, text = "Dispatch Items API")
+        submissions_push_notifications_api_title.grid(column = 7, row = 0)
+      
+    def gui_tally_responses(self):
+        tallied = Submissions_API(self.account_info, self.formid.get())
+        tallied.tally_responses()
+
+    def gui_visit_submissions_xml(self):
+        tallied = Submissions_API(self.account_info, self.formid.get())
+        tallied.visit_api()
+
+    def create_console(self):
+        large_console = tkinter.scrolledtext.ScrolledText(self.mainframe)
+        large_console.grid(column = 0, row = 8, columnspan = 2)
+
+mainWindow = tkinter.Tk()
+SurveyorInterface(mainWindow)
+mainWindow.mainloop()
